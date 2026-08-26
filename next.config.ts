d42c9810d44c9c225c8202142322fd54cca84a38
ChikaRin <chikarin@MacBook-Air-cua-ChikaRin.local>
2026-08-26 22:11:41 +0700

    Update Next.js configuration to enable static HTML/CSS export and set base path for the project.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "export", // Bắt buộc để build ra HTML/CSS tĩnh
+  basePath: "/Nekotic-Workspace",
   images: {
     remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
   },
