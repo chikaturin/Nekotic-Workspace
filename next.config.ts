94aff1ddfe301ef60eb935bf2f220dd09359a1e2
ChikaRin <chikarin@MacBook-Air-cua-ChikaRin.local>
2026-08-26 22:36:51 +0700

    feat: config github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "export", // Bắt buộc để build ra HTML/CSS tĩnh
+  output: "export",
+  trailingSlash: true, // BẮT BUỘC: để Next.js export dạng /folder/index.html thay vì /folder.html
   basePath: "/Nekotic-Workspace",
   images: {
+    unoptimized: true, // BẮT BUỘC: GitHub Pages không chạy được Node.js Image Optimizer
     remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
   },
 };
